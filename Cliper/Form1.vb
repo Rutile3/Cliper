c9170fef7d1d3fa5929a4a67b61056357e00e57a
Rutile_Darkness3_for_DESKTOP-8MKUBO2 <Rutile3@outlook.jp>
2018-05-26 22:56:42 +0900

    マウスカーソルに合わせてListBoxのインデックスを変更する処理を作成

--- Cliper/Form1.vb
+++ Cliper/Form1.vb
@@ -5,6 +5,7 @@
 
         If Text = Nothing Then
             Text = "InitListBox"
+            Clipboard.SetText("InitListBox")
         End If
 
         ClipBoardList.Items.Add(Text)
@@ -17,6 +18,7 @@
 
     Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
         Dim Text As String = ClipBoardList.SelectedItem
+
         Clipboard.SetText(Text)
         LastSetText = Text
     End Sub
@@ -34,11 +36,17 @@
     End Sub
 
     Private Sub ClipBoardList_MouseMove(sender As Object, e As MouseEventArgs) Handles ClipBoardList.MouseMove
+        Dim Index As Integer = (e.Y - 7) / 16 '本当にクライアントか？
 
+        If ClipBoardList.Items.Count <= Index Then
+            Index = ClipBoardList.Items.Count - 1
+        End If
+        ClipBoardList.SelectedIndex = Index
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim Text As String = Clipboard.GetText()
+
         If LastSetText <> Text Then
             If ClipBoardList.Items.IndexOf(Text) <> -1 Then '重複の除外
                 ClipBoardList.Items.Remove(Text)
